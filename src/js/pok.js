30085a153783eb008df162e7ae2cd4c76d459e15
Vincent Bernard <vincentbernard100@gmail.com>
2016-12-07 14:17:44 -0500

    Ajustements (presque finaux)

--- src/js/pok.js
+++ src/js/pok.js
@@ -1,55 +1,16 @@
 var jQuery = require('jquery');
 var $ = jQuery
 
-/**Show advanced controls*/
-$(document).on("click","#Advanced", function(){
-
-    if (document.getElementById('Advanced').classList.contains('off')){
-        $(".advanced").attr('style', 'opacity:100; height:auto;');
-        document.cookie = ('advanced=1');
-    }else{
-        $(".advanced").attr('style', 'opacity:0; height:0px;');
-        document.cookie = ('advanced=0');
-    };
-
-    this.classList.toggle('off');
-});
-
-/**Show basic controls*/
-$(document).on("click","#Basic", function(){
-
-    if (document.getElementById('Basic').classList.contains('off')){
-        $(".basic").attr('style', 'opacity:100; height:auto;');
-        document.cookie = ('basic=1');
-    }else{
-        $(".basic").attr('style', 'opacity:0; height:0px;');
-        document.cookie = ('basic=0');
-    };
-    this.classList.toggle('off');
-});
-
-
-// CLEAR SEARCH
-$(document).on("click","#Clear", function(){
-  location.href = "#"
-  $("#search > div > div:nth-child(2) > div:nth-child(1) > div.col-md-8 > div.CoveoOmnibox.magic-box.magic-box-inline.coveo-magicbox-search-as-you-type > div.magic-box-input > input").focus()
-});
-
 $(document).ready(function(){
-
-    if (getCookie('basic')==1){
-        $("#Basic").click();
-    };
-
-    if (getCookie('advanced')==1){
-        $("#Advanced").click();
-    };
 
     /**Randomize gif*/
     var nGif = Math.floor(Math.random() * 12) + 1 ;
     var nTxt = Math.floor(Math.random() * 12) + 1 ;
     $("#gif").attr('src', 'img/gif' + nGif + '.gif');
     $("#giftxt").attr('src', 'img/txt' + nTxt + '.png');
+
+    // put about.html in the placeholder
+    $(".abo").load("about");
 
 });
 
